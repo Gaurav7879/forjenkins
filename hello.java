009b69f6934f2b10e7c8c51944b555775a1ac92a
Gaurav7879 <101170656+Gaurav7879@users.noreply.github.com>
2025-11-11 00:26:19 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello from Git + EC2!");
-	System.out.println("My name is gaurav rahul mali");
+	System.out.println("My name is gaurav rahul Mali");
 	System.out.println("Its updated code from my team");
     }
 }
